0a19b9467423dc818a8c270f18a47ef1518f5a11
Erick-Rangel <rebel863@hotmail.com>
2023-02-07 12:25:44 -0600

    first commit

---
+++ routes/transactions.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const router = Router();
+
+const {
+  createNewTransaction,
+  getTransactions,
+  getTransactionsById,
+  getTransactionsMonth,
+  deleteTransactions,
+  getTransactionsOfUser,
+  getTransactionsMonthOfUser,
+  getPdf,
+} = require("../controllers/transactions.controller");
+
+router.route("/").post(createNewTransaction);
+
+router.route("/").get(getTransactions);
+
+router.route("/allusers/:month").get(getTransactionsMonth);
+
+// router.route("/deleteAll/:delete").delete(deleteTransactions);
+// router.delete("/", deleteTransaction);
+
+module.exports = router;
