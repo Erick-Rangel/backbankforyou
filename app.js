97fc9f2771d7f7f3083e256ad03ca94fb85e5dba
Erick-Rangel <rebel863@hotmail.com>
2023-02-07 23:22:23 -0600

    a

--- app.js
+++ app.js
@@ -10,11 +10,7 @@ app.set("port", process.env.PORT || 4000);
 app.use(
   cors({
     origin: [
-      "https://bankforyou.vercel.app",
       "https://solucionesintegralesentecnologia.com",      
-      "http://localhost:3000",
-      "https://localhost:3000",
-      "http://192.168.2.205:3000",
     ],
   })
 ); //cors hace q dos servidores se conecten entre ellos (back y front por ej)
