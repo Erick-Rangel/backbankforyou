395222820c13d3c3dcc40386423f2a8a9ed4d7bc
Erick-Rangel <rebel863@hotmail.com>
2023-02-08 22:21:06 -0600

    regresando el estado

--- app.js
+++ app.js
@@ -8,8 +8,12 @@ app.set("port", process.env.PORT || 4000);
 
 //middlewares
 app.use(
-  cors()
-  ); //cors hace q dos servidores se conecten entre ellos (back y front por ej)
+  cors({
+    origin: [
+      "https://solucionesintegralesentecnologia.com",      
+    ],
+  })
+); //cors hace q dos servidores se conecten entre ellos (back y front por ej)
 app.use(express.json()); //para q las rutas entiendan json y strings
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
